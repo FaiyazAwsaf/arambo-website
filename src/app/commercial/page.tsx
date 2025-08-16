4caed495a8f7f6ae3797df4674fc84750a04300b
shihabhq <adnansaim15@yahoo.com>
2025-08-17 00:38:48 +0600

    image adding to commercial page

--- src/app/commercial/page.tsx
+++ src/app/commercial/page.tsx
@@ -2,9 +2,18 @@ import { ArrowRight, Home, Search } from "lucide-react";
 
 // Replicate  https://www.arambo.co/comercial
 const CommercialPage = () => {
-  return <section>
-    
-  </section>;
+  return (
+    <section
+      className="related w-full py-32 rounded-2xl overflow-hiddenbg-cover bg-center"
+      style={{ backgroundImage: "url('/commercial/commercial-bg.png')" }}
+    >
+      <img
+        src="/commercial/commercial-bg.png"
+        alt="Commercial Space"
+        className="absolute inset-0 w-full h-full object-cover"
+      />
+    </section>
+  );
 };
 
 export default CommercialPage;
