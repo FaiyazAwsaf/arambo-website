73a18b97553b2526b82567bd5f017aff3a778765
shihabhq <adnansaim15@yahoo.com>
2025-08-17 00:16:46 +0600

    global css color added

--- src/app/commercial/page.tsx
+++ src/app/commercial/page.tsx
@@ -1,1 +1,10 @@
+import { ArrowRight, Home, Search } from "lucide-react";
+
 // Replicate  https://www.arambo.co/comercial
+const CommercialPage = () => {
+  return <section>
+    
+  </section>;
+};
+
+export default CommercialPage;
