093e3403b6c211e51b8f42699012cb2328dc112e
Faiyaz Awsaf <faiyazawsaf11@gmail.com>
2025-08-17 18:36:43 +0600

    Footer Initialization

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Footer = () => {
-  return <footer>{/* Footer content goes here */}</footer>;
+  return <footer>This is the footer</footer>;
 };
 
 export default Footer;
