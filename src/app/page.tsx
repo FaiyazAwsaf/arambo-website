fec98d5a0294903c98371a76a5fc74587df77b1f
NafisAhnnaf <nafisahnaf@yahoo.com>
2025-08-20 01:36:33 +0600

    Homepage Image Carousel Section and Know property section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { faCircleArrowRight, faChevronRight, faCircleCheck } from '@fortawesome/
 import building from "/building.png"
 import AutoScrollCarousel from '@/components/AutoScrollCarousel'
 import Toggle from '@/components/Toggle'
+import CarouselCard from '@/components/CarouselCard'
 // import banner from "@/app/"
 const page = () => {
 
@@ -200,16 +201,58 @@ const page = () => {
             </div >
             <div className="p-base text-Arambo-Text">Find your ideal apartment by applying filters for bachelors, women, and families, plus options for furnished or non-furnished units.</div>
             <div className='flex flex-wrap gap-6'>
-              <button className={`py-3 px-6 ${`text-Arambo-White bg-Arambo-Accent`} rounded-full`}>Family</button>
-              <button className={`py-3 px-6 ${`text-Arambo-Black bg-Arambo-Background`} rounded-full`}>Bachelor</button>
-              <button className={`py-3 px-6 ${`text-Arambo-Black bg-Arambo-Background`} rounded-full`}>Women</button>
-              <button className={`py-3 px-6 ${`text-Arambo-Black bg-Arambo-Background`} rounded-full`}>Furnished</button>
-              <button className={`py-3 px-6 ${`text-Arambo-White bg-Arambo-Accent`} rounded-full`}>Non-furnished</button>
+              <input value={"Family"} type='button' className={`py-3 px-6 ${`text-Arambo-White bg-Arambo-Accent`} rounded-full`} />
+              <input value={"Women"} type='button' className={`py-3 px-6 ${`text-Arambo-Black bg-Arambo-Background`} rounded-full`} />
+              <input value={"Bachelor"} type='button' className={`py-3 px-6 ${`text-Arambo-Black bg-Arambo-Background`} rounded-full`} />
+              <input value={"Furnished"} type='button' className={`py-3 px-6 ${`text-Arambo-Black bg-Arambo-Background`} rounded-full`} />
+              <input value={"Non-Furnished"} type='button' className={`py-3 px-6 ${`text-Arambo-White bg-Arambo-Accent`} rounded-full`} />
             </div>
           </div>
 
           <div className=''>
             Cards Carousal
+          </div>
+        </div>
+      </section>
+      <section className='space-y-12 py-24 flex justify-center'>
+        <CarouselCard />
+      </section>
+      <section className='space-y-12 py-24 flex justify-center bg-Arambo-White'>
+        <div className='bg-Arambo-Background rounded-2xl md:w-[1200px] flex justify-center'>
+          <div className='md:w-[1112px] py-8 px-5 space-y-16 flex flex-col justify-center' >
+            <div className='grid md:grid-cols-2 grid-cols-1'>
+              <div>
+                <h2 className='h2 pr-3'>Know Your Property’s
+                  True Worth</h2>
+              </div>
+              <div className='space-y-8'>
+                <p className='p-base text-Arambo-Text'>You get access to verified legal professionals who can review contracts, handle documentation, and guide you through every legal step of your real estate journey.</p>
+                <button className='px-10 py-4 bg-Arambo-Accent text-Arambo-White button-label rounded-lg'>Know property worth</button>
+              </div>
+            </div>
+            <div className='flex md:flex-row flex-col justify-center space-x-4 '>
+              <div className='rounded-xl bg-Arambo-White py-8 px-6 space-y-8'>
+                <div><img src="/HouseIcon2.svg" alt="Icon" /></div>
+                <div className='space-y-3 mt-7'>
+                  <h5 className='h5'>Submit property details</h5>
+                  <p className="p-base text-Arambo-Text">Fill out a quick form with your property’s location, size, type, and condition.</p>
+                </div>
+              </div>
+              <div className='rounded-xl bg-Arambo-White py-8 px-6 space-y-8'>
+                <div><img src="/VerifiedIcon2.svg" alt="Icon" /></div>
+                <div className='space-y-3 mt-7'>
+                  <h5 className='h5'>Submit property details</h5>
+                  <p className="p-base text-Arambo-Text">Fill out a quick form with your property’s location, size, type, and condition.</p>
+                </div>
+              </div>
+              <div className='rounded-xl bg-Arambo-White py-8 px-6 space-y-8'>
+                <div><img src="/CheckBoardIcon.svg" alt="Icon" /></div>
+                <div className='space-y-3 mt-7'>
+                  <h5 className='h5'>Submit property details</h5>
+                  <p className="p-base text-Arambo-Text">Fill out a quick form with your property’s location, size, type, and condition.</p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </section>
