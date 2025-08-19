fe0e67d001f5d3e20875134c15c464a7061c1717
Faiyaz Awsaf <faiyazawsaf11@gmail.com>
2025-08-20 04:12:35 +0600

    added routing - truck booking page

--- src/components/homepageComponents/DynamicCard.tsx
+++ src/components/homepageComponents/DynamicCard.tsx
@@ -24,8 +24,9 @@ const DynamicCard: React.FC<DynamicCardProps> = ({ index }) => {
         <div className={`border-t-2 p-5 ${index === 1 ? `border-Arambo-Accent` : `border-Arambo-Background`} space-y-4`}>
             <h4 className='h4 font-bold'>Interior Decoration</h4>
             <p className='p-base text-[#000000A3]'>Connect with our design experts today and discover how our bespoke interior decor solutions can elevate your environment.</p>
-            {/* Use Link from React Router Dom instead */}
-            <button className='border-b-2 border-Arambo-Accent'>Contact Us</button>
+            <a href="">
+                <button className='border-b-2 border-Arambo-Accent'>Contact Us</button>
+            </a>
         </div>
     </div>
 }
