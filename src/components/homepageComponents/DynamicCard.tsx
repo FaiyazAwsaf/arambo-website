e4cfb8e2a603d2ef85b9039e2d7318773b186723
Faiyaz Awsaf <faiyazawsaf11@gmail.com>
2025-08-20 04:09:32 +0600

    added routing - truck booking page

--- src/components/homepageComponents/DynamicCard.tsx
+++ src/components/homepageComponents/DynamicCard.tsx
@@ -11,7 +11,9 @@ const DynamicCard: React.FC<DynamicCardProps> = ({ index }) => {
             <h4 className='h4 font-bold'>Truck Moving</h4>
             <p className='p-base text-[#000000A3]'>Whether you`re relocating a home or business, our professional truck moving service ensures secure, on-time delivery every time.</p>
             {/* Use Link from React Router Dom instead */}
-            <button className='border-b-2 border-Arambo-Accent'>Book Now</button>
+            <a href="/book-a-truck">
+                <button className='border-b-2 border-Arambo-Accent'>Book Now</button>
+            </a>
         </div>
         <div className={`border-t-2 p-5 ${index === 0 ? `border-Arambo-Accent` : `border-Arambo-Background`} space-y-4`}>
             <h4 className='h4 font-bold'>Furniture on Demand</h4>
