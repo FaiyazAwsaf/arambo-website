421addc285a57a0314249b21d773dfd28eff66ce
Faiyaz A Awsaf <64351054+FaiyazAwsaf@users.noreply.github.com>
2025-08-21 23:54:26 +0600

    Module/agents (#23)
    * Development (#20)
    
    * Complete Header Implementation
    
    * Header Initialization
    
    * Header dropdown implementation
    
    * Header dropdown alignment
    
    * Header Implementation
    
    * Module/commercial page (#16)
    
    * image adding to commercial page
    
    * initial hero section added
    
    * Partially done Residential Page
    
    * Partially done Residential Page
    
    * Filter and Card
    
    * Complete Header Implementation (#12)
    
    * Header Initialization
    
    * Header dropdown implementation
    
    * Header dropdown alignment
    
    * Header Implementation
    
    * commercial page data changed
    
    * Merge branch 'module/residential_page' of github.com:FaiyazAwsaf/arambo-website into module/commercial_page
    
    * commercial page components added
    
    ---------
    
    Co-authored-by: shihabhq <adnansaim15@yahoo.com>
    Co-authored-by: Nafis <nafisahnaf@yahoo.com>
    
    * Module/commercial page (#18)
    
    * image adding to commercial page
    
    * initial hero section added
    
    * Partially done Residential Page
    
    * Partially done Residential Page
    
    * Filter and Card
    
    * Complete Header Implementation (#12)
    
    * Header Initialization
    
    * Header dropdown implementation
    
    * Header dropdown alignment
    
    * Header Implementation
    
    * commercial page data changed
    
    * Merge branch 'module/residential_page' of github.com:FaiyazAwsaf/arambo-website into module/commercial_page
    
    * commercial page components added
    
    * Module/commercial page (#15)
    
    * image adding to commercial page
    
    * initial hero section added
    
    * Partially done Residential Page
    
    * Partially done Residential Page
    
    * Filter and Card
    
    * commercial page data changed
    
    * Merge branch 'module/residential_page' of github.com:FaiyazAwsaf/arambo-website into module/commercial_page
    
    * commercial page components added
    
    ---------
    
    Co-authored-by: Nafis <nafisahnaf@yahoo.com>
    Co-authored-by: Faiyaz A Awsaf <64351054+FaiyazAwsaf@users.noreply.github.com>
    
    * Update globals.css
    
    * reusable components for residential and commercial page done
    
    ---------
    
    Co-authored-by: shihabhq <adnansaim15@yahoo.com>
    Co-authored-by: Nafis <nafisahnaf@yahoo.com>
    
    * Update globals.css
    
    * Home page complete implementation
    
    * Homepage
    
    * Homepage AutoScrollCarousel Component
    
    * Homepage upto Arambo Supports
    
    * Homepage Fixed Icons in some cards
    
    * Homepage Legal Section and Hyper Filter Section
    
    * Homepage Image Carousel Section and Know property section
    
    * Homepage NewsMedia Section
    
    ---------
    
    Co-authored-by: NafisAhnnaf <nafisahnaf@yahoo.com>
    
    ---------
    
    Co-authored-by: shihabhq <adnansaim15@yahoo.com>
    Co-authored-by: Nafis <nafisahnaf@yahoo.com>
    
    * Agents page complete
    
    * Agents details page complete
    
    * Agents details page complete with KnowYourProperty.tsx component
    
    ---------
    
    Co-authored-by: shihabhq <adnansaim15@yahoo.com>
    Co-authored-by: Nafis <nafisahnaf@yahoo.com>

---
+++ src/components/homepageComponents/KnowYourProperty.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+const KnowYourProperty = () => {
+    return (
+        <div className='bg-Arambo-Background rounded-2xl md:w-[1200px] flex justify-center'>
+            <div className='md:w-[1112px] py-8 px-5 space-y-16 flex flex-col justify-center' >
+                <div className='grid md:grid-cols-2 grid-cols-1'>
+                    <div>
+                        <h2 className='h2 pr-3'>Know Your Property’s
+                            True Worth</h2>
+                    </div>
+                    <div className='space-y-8'>
+                        <p className='p-base text-Arambo-Text'>You get access to verified legal professionals who can review contracts, handle documentation, and guide you through every legal step of your real estate journey.</p>
+                        <button className='px-10 py-4 bg-Arambo-Accent text-Arambo-White button-label rounded-lg'>Know property worth</button>
+                    </div>
+                </div>
+                <div className='flex md:flex-row flex-col justify-center space-x-4 '>
+                    <div className='rounded-xl bg-Arambo-White py-8 px-6 space-y-8'>
+                        <div><img src="/homepageAssets/HouseIcon2.svg" alt="Icon" /></div>
+                        <div className='space-y-3 mt-7'>
+                            <h5 className='h5'>Submit property details</h5>
+                            <p className="p-base text-Arambo-Text">Fill out a quick form with your property’s location, size, type, and condition.</p>
+                        </div>
+                    </div>
+                    <div className='rounded-xl bg-Arambo-White py-8 px-6 space-y-8'>
+                        <div><img src="/homepageAssets/VerifiedIcon2.svg" alt="Icon" /></div>
+                        <div className='space-y-3 mt-7'>
+                            <h5 className='h5'>Submit property details</h5>
+                            <p className="p-base text-Arambo-Text">Fill out a quick form with your property’s location, size, type, and condition.</p>
+                        </div>
+                    </div>
+                    <div className='rounded-xl bg-Arambo-White py-8 px-6 space-y-8'>
+                        <div><img src="/homepageAssets/CheckBoardIcon.svg" alt="Icon" /></div>
+                        <div className='space-y-3 mt-7'>
+                            <h5 className='h5'>Submit property details</h5>
+                            <p className="p-base text-Arambo-Text">Fill out a quick form with your property’s location, size, type, and condition.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default KnowYourProperty
