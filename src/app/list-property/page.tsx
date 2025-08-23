c20235394a366cc250fbb16fe66f470017754be3
Faiyaz A Awsaf <64351054+FaiyazAwsaf@users.noreply.github.com>
2025-08-24 02:30:39 +0600

    Fix/homepage (#33)
    * Responsive fix and Interactivity
    
    * Responsive fix and Interactivity fix
    
    * Responsive fix and Interactivity fix
    
    * Responsive fix and Interactivity fix
    
    * build fix
    
    ---------
    
    Co-authored-by: NafisAhnnaf <nafisahnaf@yahoo.com>

--- src/app/list-property/page.tsx
+++ src/app/list-property/page.tsx
@@ -6,7 +6,7 @@ const page = () => {
 
     return (
         <div className="px-4 bg-Arambo-Background">
-            <div className=" h-[296px] rounded-2xl bg-[linear-gradient(180deg,#000B26_0%,#00123C_19.69%,#032471_70.33%,#0C39A3_86.64%,#0041D9_100%)] flex justify-center items-center">
+            <div className=" h-[296px] rounded-2xl bg-[linear-gradient(180deg,#000B26_0%,#00123C_19.69%,#032471_70.33%,#0C39A3_86.64%,#0041D9_100%)] flex justify-center items-center text-center">
                 <h1 className="h1 text-Arambo-White">List Your Property</h1>
             </div>
             <div className="py-20 bg-Arambo-Background flex justify-center items-center ">
@@ -16,7 +16,7 @@ const page = () => {
                             <h2 className="h2">What would you like to do?</h2>
                             <p className="label-18 max-w-md">Choose how you want to get started — rent or sell in just a click.</p>
                         </div>
-                        <div className="grid md:grid-cols-2 grid-cols-1 gap-x-6 ">
+                        <div className="grid md:grid-cols-2 grid-cols-1 gap-6 ">
                             <div className="bg-Arambo-Background px-8 py-11 flex flex-col justify-center items-center space-y-8 rounded-2xl">
                                 <div className="text-center flex flex-col justify-center items-center space-y-3">
                                     <h4 className="h4">Rent Out a Property</h4>
